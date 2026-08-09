cdec05626d850f24512885c97305ee33620d2fe3
nafissiddiky2 <nafissiddiky2@gmail.com>
2026-08-10 00:27:51 +0600

    updating the Google Apps Script URL

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,5 @@
 // Google Sheets Web App URL
-const STUDENT_SHEET_URL = 'https://script.google.com/macros/s/AKfycbyHSMI-w9mhIHVA6Z3NWFJbc0o9xr46l6jOSkK0q1fjyaoBuJhnyjr_6s95CHdXLYmstQ/exec';
+const STUDENT_SHEET_URL = 'https://script.google.com/macros/s/AKfycbxYj2YzzP47VpFQTRuvA5fbcy8cF895k1Tb94a_ZGKSFDMMeC93UB1BKtqCgq0qElSb3Q/exec';
 
 // Login Form Handler
 document.getElementById('loginForm')?.addEventListener('submit', async (e) => {
